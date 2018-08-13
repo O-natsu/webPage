f9ace82db55de477231992a81bb7f4cf6a1511b8
O-natsu <ym.natsubori@gmail.com>
2018-08-13 19:59:08 +0900

    [add] webPage
    以前作ったものを手直ししてコミット
    
    Signed-off-by: O-natsu <ym.natsubori@gmail.com>

---
+++ src/js/loopSlider.js
@@ -0,0 +1,43 @@
+﻿$(function () {
+    // 1周するまでの時間
+    let slideSec = 25000;
+
+    // 複数の.loop-sliderが存在するので、eachで1つ1つ処理していく
+    $(".loop-slider").each(function(index, loopSlider) {
+        const loopElem = $(loopSlider);
+        // 最初の要素の内幅(borderは除き、paddingは含む)
+        const loopElemInnnerWidth = loopElem.innerWidth();
+        const loopUlist = loopElem.find("ul");
+        const loopList = loopUlist.find("li");
+        // li要素内の外幅を取得 ≒ 画像サイズ
+        const loopListOuterWidth = loopList.outerWidth();
+        // <li>の要素分widthを計算するため要素数を取得
+        const listCount = loopList.length;
+        const loopElemWidth = loopListOuterWidth * listCount; // 2604px 217*12
+ 
+        // li要素分のwidthが外幅より大きい場合にスライドさせる
+        // div自体で見せる範囲を決めておくため、widthを狭める
+        if (loopElemWidth > loopListOuterWidth) {
+            loopUlist.css({
+                width: loopElemWidth
+            });
+            loopElem.css({
+                width: loopElemWidth / 4
+            });
+            slideAnimation();
+        }
+
+        // アニメーションを行う
+        function slideAnimation() {
+            loopElem.animate({
+                left: '-' + loopElem.width() + 'px'
+            },
+                slideSec,
+                'linear',
+                function () {
+                    loopElem.css({ left: '0' });
+                    slideAnimation();
+                });
+        };
+    });
+});
---
+++ src/js/slider.js
@@ -0,0 +1,66 @@
+﻿// 読み込み完了時に実行させる
+window.onload = function () {
+    // 変数定義
+    // 画像のプロパティ
+    var imageList = [
+        { src: './image/slide_show_normal.png', alt: '1', width: '1300', height: '720'},
+        { src: './image/slide_show_red.png', alt: '2', width: '1300', height: '720'},
+        { src: './image/slide_show_normal.png', alt: '3', width: '1300', height: '720'},
+        { src: './image/slide_show_red.png', alt: '4', width: '1300', height: '720'}
+    ];
+    var imageLength = imageList.length;
+
+    // 要素取ってくる
+    var sliderBox = document.getElementById('slider_box');
+    // タイトル取ってくる
+    //var h1Title = document.getElementById('h1_title');
+    // 現在のインデックス
+    var currentIndex = 0;
+
+    // 関数定義
+    // 指定の画像に表示を切り替える
+    function showImage(index) {
+        // 表示させずに、裏で一度隠しておくdisplay:none
+        for (var i = 0; i < imageLength; i++) {
+            imageList[i].elem.style.display = 'none';
+        }
+        // 表示する対象の要素を取ってくる
+        var targetImage = imageList[index];
+        // 表示する対象の要素を出す
+        targetImage.elem.style.display = 'inline';
+        // イベントの設定
+        setTimeout(function slide() {
+            // 表示する画像のインデックスを計算する
+            currentIndex++;
+            // 画像を表示するためのインデックスと画像プロパティのlngthが一緒になったら0に戻す
+            if (currentIndex === imageLength) {
+                currentIndex = 0;
+            }
+            // 画像を切替える
+            showImage(currentIndex);
+            // classListは引数空文字を許可しないのでclassNameで変えてやる
+            //targetImage.classList.add('')
+            targetImage.className ='';
+        }, 3000);
+    }
+    
+    // 初期化処理
+    // img要素をDOMに追加
+    for (var i = 0; i < imageLength; i++) {
+        var item = imageList[i];
+
+        // img要素の作成
+        var img = document.createElement('img');
+        img.src = item.src;
+        img.alt = item.alt;
+        img.width = item.width;
+        img.height = item.height;
+
+        // 作成したimg要素をDOMにあるdivに追加する
+        // <div id="slider_box"><img/></div>
+        sliderBox.appendChild(img);
+        item.elem = img;
+    }
+    // 初期化
+    showImage(currentIndex);
+};
